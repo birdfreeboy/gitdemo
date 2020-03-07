508af7e906691822cc394322173748b68d30655c
18468 <18468@LAPTOP-28TGE55Q>
2020-03-07 20:14:37 +0800

    okk

---
+++ gitdemo/Program - 复制.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace gitdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //TO DO 
+            //first 
+        }
+    }
+}
