69ba3d16436007a17870171cfba751db814d1ac8
18468 <18468@LAPTOP-28TGE55Q>
2020-03-07 22:33:59 +0800

    2

--- gitdemo/Program.cs
+++ gitdemo/Program.cs
@@ -6,7 +6,7 @@ namespace gitdemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! 2");
             //TO DO 
             //first 
         }
